b04a1410aa4369978ed1de52d603f817b37e2d95
Eric West <esw9999@gmail.com>
2014-12-23 03:29:19 -0600

    Much complexity, much collection
    Introduces the ability to have collections of properties built from
    other structs and representer modules.

--- lib/kitsune/json.ex
+++ lib/kitsune/json.ex
@@ -4,6 +4,7 @@ defmodule Kitsune.JSON do
     quote do
       import Kitsune.JSON
       @properties []
+      @collections []
       @before_compile Kitsune.JSON
     end
   end
@@ -58,21 +59,91 @@ defmodule Kitsune.JSON do
     end
   end
 
-  defmacro __before_compile__(env) do
+  @doc """
+  Nest a collection of other representations in the output.
+
+  ## Examples
+
+      defmodule Album do
+        defstruct name: nil, songs: []
+      end
+
+      defmodule AlbumRepresenter do
+        use Kitsune.JSON
+
+        property :name
+        collection :songs, extend: SongRepresenter
+      end
+
+      album = %Album{name: "Doggystyle", songs: [song, %Song{name: "Lodi Dodi"}]}
+      AlbumRepresenter.to_json(album)
+      #=> "{\"name\":\"Doggystyle\",\"songs\":[{\"title\":\"Gin and Juice\"},{\"title\":\"Lodi Dodi\"}]}"
+
+  """
+  defmacro collection(label, opts \\ []) do
+    as = Keyword.get(opts, :as, label)
+    extend = Keyword.get(opts, :extend)
+    from = Keyword.get(opts, :from)
+    quote do
+      @collections [%{
+                      label: unquote(label),
+                      as: unquote(as),
+                      extend: unquote(extend),
+                      from: unquote(from)
+                     } | @collections ]
+    end
+  end
+
+  defmacro __before_compile__(_env) do
     quote do
       def to_json(struct) do
-        map = for {label, as} <- @properties, into: %{} do
-          { label, Map.get(struct, as) }
-        end
+        map =  _map_from_collections(struct)
+            |> _map_from_properties(struct)
         {:ok, json} = Poison.encode(map)
         json
       end
 
-      def from_json(json, mod) do
+      defp _map_from_collections(struct) do
+        for %{label: label, as: as, extend: extend} <- @collections, into: %{} do
+          { label, Enum.map( Map.get(struct, as), fn(x) ->
+            extend.to_json(x) |> _unjson
+          end) }
+        end
+      end
+
+      defp _map_from_properties(map, struct) do
+        for {label, as} <- @properties, into: map do
+          { label, Map.get(struct, as) }
+        end
+      end
+
+      defp _unjson(json) do
         {:ok, decoded} = Poison.decode(json)
+        decoded
+      end
+
+      def from_json(json, mod) do
+        decoded = _unjson(json)
         map = for {label, as} <- @properties, into: %{} do
           { as, Map.get(decoded, Atom.to_string(label)) }
         end
+
+        collections = for %{label: label, as: as, extend: extend, from: from} <- @collections, into: %{} do
+          { as,
+            Map.get(decoded, Atom.to_string(label))
+            |> Enum.map(fn(x) ->
+              {:ok, struct_json} = Poison.encode(x)
+              extend.from_json(struct_json, from)
+            end)
+          }
+        end
+
+        Enum.map(@collections, fn(c) ->
+          map = Map.pop(map, Atom.to_string(c[:label]))
+        end)
+
+        map = Map.merge(map, collections)
+
         struct(mod, map)
       end
     end
--- test/kitsune/json_test.exs
+++ test/kitsune/json_test.exs
@@ -7,16 +7,28 @@ defmodule SongRepresenter do
 
   property :title, as: :name
 end
- 
+
+defmodule Album do
+  defstruct name: nil, songs: []
+end
+
+defmodule AlbumRepresenter do
+  use Kitsune.JSON
+
+  property :name
+  collection :songs, extend: SongRepresenter, from: Song
+end
+
 defmodule JSONTest do
   use ExUnit.Case
 
   setup do
     song = %Song{name: "Gin and Juice"}
-    { :ok, song: song }
+    album = %Album{name: "Doggystyle", songs: [song, %Song{name: "Lodi Dodi"}]}
+    { :ok, song: song, album: album }
   end
 
-  test "Transforms data to json", %{song: song} do   
+  test "Transforms data to json", %{song: song} do
     json = SongRepresenter.to_json(song)
     assert json == "{\"title\":\"Gin and Juice\"}"
   end
@@ -24,5 +36,15 @@ defmodule JSONTest do
   test "Transforms data from json", %{song: song} do
     json = "{\"title\":\"Gin and Juice\"}"
     assert SongRepresenter.from_json(json, Song) == song
+  end
+
+  test "Collection to json", %{song: _song, album: album} do
+    json = "{\"songs\":[{\"title\":\"Gin and Juice\"},{\"title\":\"Lodi Dodi\"}],\"name\":\"Doggystyle\"}"
+    assert AlbumRepresenter.to_json(album) == json
+  end
+
+  test "Collection from json", %{song: _song, album: album} do
+    json = "{\"songs\":[{\"title\":\"Gin and Juice\"},{\"title\":\"Lodi Dodi\"}],\"name\":\"Doggystyle\"}"
+    assert AlbumRepresenter.from_json(json, Album) == album
   end
 end
